4ec335f403d7be332c7928f15749d318d223dea9
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-30 20:16:25 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -3,14 +3,5 @@
 int main() {
 	system("chcp 1251");
 	system("cls");
-	int n;
-	cout << "Введіть число: ";
-	cin >> n;
-	if (isPrime(n)) {
-		cout << n << "є простим числом\n" << endl;
-	}
-	else {
-		cout << n << "не є простим числом\n" << endl;
-	}
-	return 0;
+	
 }
