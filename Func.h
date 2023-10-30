e665ed2a61a13a70d520a2ce04c6649568112f21
Lutsenko Vladyslav Serhiyovych <143324701+VladislavLut@users.noreply.github.com>
2023-10-30 20:35:48 +0200

    Update Func.h

--- Func.h
+++ Func.h
@@ -3,5 +3,9 @@
 
 using namespace std;
 
-void printRectangle();
-
+void printRectangle(int height, int width);
+int factorial(int n);
+bool isPrime(int n);
+int cube(int n);
+int main();
+bool isPositive(int value);
