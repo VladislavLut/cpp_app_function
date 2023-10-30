ada85c5e4bfd99c8f9fe25d40cc456aa5be8dc79
Lutsenko Vladyslav Serhiyovych <143324701+VladislavLut@users.noreply.github.com>
2023-10-30 20:35:15 +0200

    Update Func.cpp

--- Func.cpp
+++ Func.cpp
@@ -1,11 +1,45 @@
-#include <iostream>
-#include <Windows.h>
+#include "Func.h"
 
-using namespace std;
+void printRectangle(int height, int width) {
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			cout << "* ";
+		}
 
-void printRectangle(int height, int width);
-int factorial(int n);
-bool isPrime(int n);
-int cube(int n);
-int main();
-bool isPositive(int value);
+		cout << endl;
+	}
+}
+int factorial(int n) {
+
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * factorial(n - 1);
+	}
+}
+bool isPrime(int n) {
+	if (n < 2) {
+		return false;
+	}
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+int cube(int n) {
+	return n * n * n;
+}
+int main() {
+	int a, b;
+}
+bool isPositive(int value) {
+	if (value >= 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
