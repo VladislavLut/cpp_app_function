7aa45faa060d582234e26674a6efe175607d57c9
Lutsenko Vladyslav Serhiyovych <143324701+VladislavLut@users.noreply.github.com>
2023-10-30 20:34:35 +0200

    Update Func.cpp

--- Func.cpp
+++ Func.cpp
@@ -1,4 +1,11 @@
-#include "Func.h"
+#include <iostream>
+#include <Windows.h>
 
+using namespace std;
 
-void printRactangle();
+void printRectangle(int height, int width);
+int factorial(int n);
+bool isPrime(int n);
+int cube(int n);
+int main();
+bool isPositive(int value);
